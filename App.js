aba55c5f8d934d7495b3094c1557dca28e5353eb
ngamdarunee <s6135512038@phuket.psu.ac.th>
2020-09-19 21:22:12 +0700

    เพิ่มหน้าApp

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Weather from './components/Weather';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ZipCodeScreen from "./components/ZipCodeScreen";
+import WeatherScreen from "./components/WeatherScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110" />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    //alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
