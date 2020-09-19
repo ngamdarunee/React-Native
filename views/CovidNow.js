7739b78add4cd5f0decaea9003f883ca42d33d95
ngamdarunee <s6135512038@phuket.psu.ac.th>
2020-09-19 23:51:19 +0700

    แสดงยอดต่างๆของผู้ติดเชื้อCovid19

--- views/CovidNow.js
+++ views/CovidNow.js
@@ -33,14 +33,25 @@ const CovidNow = () => {
           {CovidData.NewConfirmed}
         </Text>
       </View>
-      <Text>NOW</Text>
+      <Text style={{ fontSize: 30 }}>NOW</Text>
+      <View style={style.box}>
+        <Text>Confirmed: {CovidData.Confirmed}</Text>
+        <Text>Recovered: {CovidData.Recovered}</Text>
+        <Text>Hospitalized: {CovidData.Hospitalized}</Text>
+        <Text>Deaths: {CovidData.Deaths}</Text>
+        <Text>NewRecovered: {CovidData.NewRecovered}</Text>
+        <Text>NewDeaths: {CovidData.NewDeaths}</Text>
+      </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
   container: {
+    flexDirection: "column",
+    flex: 1,
     alignItems: "center",
+    justifyContent: "space-around",
   },
   circle: {
     width: 100,
@@ -49,6 +60,14 @@ const style = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 100,
+  },
+  box: {
+    backgroundColor: "pink",
+    height: 200,
+    width: 250,
+    padding: 22,
+    borderRadius: 20,
+    justifyContent: "space-evenly",
   },
 });
 export default CovidNow;
